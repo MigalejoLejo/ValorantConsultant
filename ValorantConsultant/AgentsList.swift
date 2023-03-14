b955d9bcd01c26fb770dcbfa779e3469cc80e301
MigalejoLejo <migalejo85@gmail.com>
2023-03-15 00:44:58 +0100

    initial commit

---
+++ ValorantConsultant/AgentsList.swift
@@ -0,0 +1,23 @@
+//
+//  AgentsList.swift
+//  ValorantConsultant
+//
+//  Created by Miguel Correa Avila on 14/3/23.
+//
+
+import SwiftUI
+
+struct AgentsList: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+            .onAppear{
+                Api().getAgents()
+            }
+    }
+}
+
+struct AgentsList_Previews: PreviewProvider {
+    static var previews: some View {
+        AgentsList()
+    }
+}
--- ValorantConsultant/ContentView.swift
+++ ValorantConsultant/ContentView.swift
@@ -7,15 +7,77 @@
 
 import SwiftUI
 
-struct ContentView: View {
-    var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+struct Agent: Hashable, Codable {
+    let status: Int
+    
+    
+    
+
+}
+
+
+
+
+class ViewModel: ObservableObject{
+    @Published var agents: Agent = Agent(status:0)
+
+    func getAgents(){
+        
+        guard let url = URL(string: "https://valorant-api.com/v1/agents") else {
+            return
         }
-        .padding()
+        
+        let task = URLSession.shared.dataTask(with: url) { [weak self]
+            data, _, error in
+            guard let data = data, error == nil else {
+                return
+            }
+       
+        
+       
+        
+            //Convert to JSON
+            do {
+                let agents = try
+                JSONDecoder().decode(Agent.self, from: data)
+                DispatchQueue.main.async {
+                    self?.agents = agents
+                }
+            }
+            catch {
+                print(error)
+            }
+         
+        }
+         
+    
+        print(task)
+        task.resume()
+        
+        
+    }
+}
+
+
+
+
+struct ContentView: View {
+@StateObject var viewModel = ViewModel()
+    
+    var body: some View {
+        NavigationView {
+            List{
+                HStack{
+                    Text("Hey: "+String(viewModel.agents.status))
+                        .bold()
+                }
+                
+            }
+            .navigationTitle("Agents")
+            .onAppear{
+                viewModel.getAgents()
+            }
+        }
     }
 }
 
