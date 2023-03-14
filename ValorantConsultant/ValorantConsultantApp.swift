24c2e345bdedb00c6bba34292b4b5f1f313942b0
MigalejoLejo <migalejo85@gmail.com>
2023-03-14 22:48:11 +0100

    Initial Commit

---
+++ ValorantConsultant/ValorantConsultantApp.swift
@@ -0,0 +1,17 @@
+//
+//  ValorantConsultantApp.swift
+//  ValorantConsultant
+//
+//  Created by Miguel Correa Avila on 14/3/23.
+//
+
+import SwiftUI
+
+@main
+struct ValorantConsultantApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
